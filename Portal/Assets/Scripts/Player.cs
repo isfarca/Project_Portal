f2d9f4ecbfc01c325b6c6eeaba5df104e22b6f34
Fethi Isfarca <fisfarca@googlemail.com>
2018-03-06 18:03:18 +0100

    Spawn Blue Portal
    First attempts to spawn the blue portal correctly by pressing a button.

--- Portal/Assets/Scripts/Player.cs
+++ Portal/Assets/Scripts/Player.cs
@@ -2,13 +2,43 @@
 
 public class Player : MonoBehaviour
 {
-	// Use this for initialization
-	void Start()
+    // Reference types
+    private Ray ray;
+    private RaycastHit hit;
+    public GameObject bluePortalPrefab;
+    public GameObject redPortalPrefab;
+
+    // Update is called once per frame
+    void Update()
     {
-	}
-	
-	// Update is called once per frame
-	void Update()
-    {
+        // Declare variables
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Set the position of cursor in the scene.
+
+        // Set ray to the cursor position.
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // hit.collider.transform
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.collider.name);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Debug.Log("Spawn Collider");
+
+                // Wall(3)
+                Instantiate
+                (
+                    bluePortalPrefab,
+                    new Vector3
+                    (
+                        mousePosition.x,
+                        hit.collider.transform.position.y,
+                        hit.collider.transform.position.z - 0.8f
+                    ),
+                    Quaternion.identity
+                );
+            }
+        }
 	}
 }
