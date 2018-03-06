5ea52fd4fdb4d9a4c131aa69874756b56e2b3d5d
f.isfarca <fisfarca@googlemail.com>
2018-03-06 12:10:15 +0100

    Teleport
    Portal textures and teleporting with bugs.

---
+++ Portal/Assets/Scripts/Portal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    // Reference types
+    public Transform spawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // When the spawn point available, then spawn the player in the other portal.
+        if (spawnPoint != null)
+        {
+            // Spawn the player in the other portal:
+            other.gameObject.transform.position = spawnPoint.position;
+
+            // Rotate the player in the correct direction.
+            other.transform.Rotate
+            (
+                new Vector3
+                (
+                    other.transform.rotation.x,
+                    other.transform.rotation.y + 180,
+                    other.transform.rotation.z
+                )
+            );
+        }
+    }
+}
